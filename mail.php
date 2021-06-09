a330c6c9a38df8060636d94ddc677f8acbf79baa
albina <albinabist1993@mail.ru>
2021-06-09 16:02:33 +0300

    исправила почту

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
   $json = file_get_contents('php://input');
   $data = json_decode($json);
   $headers = "From: order@cosmetology40.ru";
-  if (mail('pierroit@yandex.ru', 'Новое обращение', $data->name . ' ' . $data->phone . ' ' . $data->message, $headers)) {
+  if (mail('natela-argwliani@mail.ru', 'Новое обращение', $data->name . ' ' . $data->phone . ' ' . $data->message, $headers)) {
     http_response_code(200);
   } else {
     http_response_code(500);
